a72c2a283701c35a07d65359e33456de24b07848
Manish Rathore <manish.rathore1102@gmail.com>
2023-09-01 23:28:20 +0530

    make it resposive and add some effects

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let onblub = document.getElementById('on')
+
+function lighton(){
+    onblub.src='on_lightbulb.png'
+    onblub.style.width='350px';
+}
+
+
+function lightoff(){
+    onblub.src='off_lightbulb.png'
+    onblub.style.width='350px';
+}
+
